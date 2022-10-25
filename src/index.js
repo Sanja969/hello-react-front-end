a8dc161091af395acb18146a6da94756d5efe570
Sanja <sanja1mandic@gmail.com>
2022-10-25 13:06:52 +0200

    Initial settup and linters

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, NavLink } from 'react-router-dom';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  <React.StrictMode>
+    <BrowserRouter>
+      <NavLink to="/" role="button">
+        <span>Home | </span>
+      </NavLink>
+      <NavLink to="/greeting" role="button">
+        <span>Greeting message</span>
+      </NavLink>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
